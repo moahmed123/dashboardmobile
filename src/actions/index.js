9ca4f5cb64eef1f5c68c3a9303d525e30311199a
mohamedalaa <mohamed_ll14@yahoo.com>
2018-12-20 22:50:25 +0200

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,9 +4,10 @@ export function GetAllData(){
     return(dispatch) => {        
         const urlAllData = "http://apihospital00.herokuapp.com/api/hotel?name=all&city=all&cat=all&admin=admin&limit=20&active=all";
         return axios.get(urlAllData,{
-            headers: {
-                'Content-Type': 'application/json;charset=UTF-8',
-                "Access-Control-Allow-Origin": "*",
+            headers: {                
+                "Access-Control-Allow-Origin": " http://127.0.0.1:3000",
+                "Access-Control-Allow-Methods": "POST",
+                "Access-Control-Allow-Headers": "Content-Type, Authorization"
             }
           })
         .then((response) => {
